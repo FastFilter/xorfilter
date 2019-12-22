0b8b5dd36c5b0ff0a17ee5e173d6246a04d6ccbc
prataprc <prataprc@gmail.com>
2019-12-22 18:52:57 +0530

    benchmark code for Populate() and Contains().

--- xorfilter_test.go
+++ xorfilter_test.go
@@ -32,5 +32,31 @@ func TestBasic(t *testing.T) {
 	fpp := float64(matches) * 100.0 / float64(falsesize)
 	fmt.Println("false positive rate ", fpp)
 	assert.Equal(t, true, fpp < 0.40)
+}
 
+func BenchmarkPopulate100000(b *testing.B) {
+	testsize := 10000
+	keys := make([]uint64, testsize, testsize)
+	for i := range keys {
+		keys[i] = rand.Uint64()
+	}
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		Populate(keys)
+	}
+}
+
+func BenchmarkContains100000(b *testing.B) {
+	testsize := 10000
+	keys := make([]uint64, testsize, testsize)
+	for i := range keys {
+		keys[i] = rand.Uint64()
+	}
+	filter := Populate(keys)
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		filter.Contains(keys[n%len(keys)])
+	}
 }
