251603552fa3d0cb6c94f0e90abc7a2cbe224d2d
Daniel Lemire <lemire@gmail.com>
2020-01-06 16:54:06 -0500

    Integration of 3 points on 2 segments Xor8Plus variant

---
+++ twosegments.go
@@ -0,0 +1,215 @@
+package xorfilter
+
+
+import (
+	"errors"
+	"math"
+)
+
+
+// Xor8 offers a 0.3% false-positive probability
+type Xor8TwoSegment struct {
+	Seed         uint64
+	BlockLength  uint32
+	Size         uint32
+	Fingerprints []uint8
+}
+
+// Contains tell you whether the key is likely part of the set
+func (filter *Xor8TwoSegment) Contains(key uint64) bool {
+	hash := mixsplit(key, filter.Seed)
+	f := uint8(fingerprint(hash))
+	r0 := uint32(hash)
+	r1 := uint32(rotl64(hash, 21))
+	r2 := uint32(rotl64(hash, 42))
+	h0 := reduce(r0, filter.BlockLength)
+	h1 := h0 ^ (reduce(r1, 63) + 1)
+	h2 := reduce(r2, filter.BlockLength) + filter.BlockLength
+	return f == (filter.Fingerprints[h0] ^ filter.Fingerprints[h1] ^
+		filter.Fingerprints[h2])
+}
+
+func (filter *Xor8TwoSegment) geth0h1h2(k uint64) hashes {
+	hash := mixsplit(k, filter.Seed)
+	answer := hashes{}
+	answer.h = hash
+	r0 := uint32(hash)
+	r1 := uint32(rotl64(hash, 21))
+	r2 := uint32(rotl64(hash, 42))
+
+	answer.h0 = reduce(r0, filter.BlockLength)
+	answer.h1 = answer.h0 ^ (reduce(r1, 63) + 1)
+	answer.h2 = reduce(r2, filter.BlockLength)
+	return answer
+}
+
+func (filter *Xor8TwoSegment) geth0(hash uint64) uint32 {
+	r0 := uint32(hash)
+	return reduce(r0, filter.BlockLength)
+}
+
+func (filter *Xor8TwoSegment) geth1(h0 uint32, hash uint64) uint32 {
+	r1 := uint32(rotl64(hash, 21))
+	return h0 ^ (reduce(r1, 63) + 1)
+}
+
+func (filter *Xor8TwoSegment) geth2(hash uint64) uint32 {
+	r2 := uint32(rotl64(hash, 42))
+	return reduce(r2, filter.BlockLength)
+}
+
+// Populate fills the filter with provided keys.
+// The caller is responsible to ensure that there are no duplicate keys.
+func PopulateTwoSegment(keys []uint64)  (*Xor8TwoSegment, error)  {
+	size := len(keys)
+	capacity := 32 + uint32(math.Ceil(1.27*float64(size)))
+	filter := &Xor8TwoSegment{}
+	filter.Size = uint32(len(keys))
+	filter.BlockLength = capacity / 2
+	filter.BlockLength = (filter.BlockLength + 63) &^ 63 // round up to 64 bit blocks
+	capacity = filter.BlockLength * 2
+	filter.Fingerprints = make([]uint8, capacity, capacity)
+	var rngcounter uint64 = 1
+	filter.Seed = splitmix64(&rngcounter)
+
+	Q0 := make([]keyindex, filter.BlockLength, filter.BlockLength)
+	Q1 := make([]keyindex, filter.BlockLength, filter.BlockLength)
+	stack := make([]keyindex, size, size)
+	sets0 := make([]xorset, filter.BlockLength, filter.BlockLength)
+	sets1 := make([]xorset, filter.BlockLength, filter.BlockLength)
+	iterations := 0
+	for true {
+		iterations += 1
+		if iterations > MaxIterations {
+			return nil, errors.New("too many iterations, you probably have duplicate keys")
+		}
+		for i := 0; i < size; i++ {
+			key := keys[i]
+			hs := filter.geth0h1h2(key)
+			sets0[hs.h0].xormask ^= hs.h
+			sets0[hs.h0].count++
+			sets0[hs.h1].xormask ^= hs.h
+			sets0[hs.h1].count++
+			sets1[hs.h2].xormask ^= hs.h
+			sets1[hs.h2].count++
+		}
+		// scan for values with a count of one
+		Q0size := 0
+		Q1size := 0
+		for i := uint32(0); i < filter.BlockLength; i++ {
+			if sets0[i].count == 1 {
+				Q0[Q0size].index = i
+				Q0[Q0size].hash = sets0[i].xormask
+				Q0size++
+			}
+		}
+
+		for i := uint32(0); i < filter.BlockLength; i++ {
+			if sets1[i].count == 1 {
+				Q1[Q1size].index = i
+				Q1[Q1size].hash = sets1[i].xormask
+				Q1size++
+			}
+		}
+		stacksize := 0
+		for Q0size+Q1size > 0 {
+			for Q0size > 0 {
+				Q0size--
+				keyindexvar := Q0[Q0size]
+				index := keyindexvar.index
+				if sets0[index].count == 0 {
+					continue // not actually possible after the initial scan.
+				}
+				hash := keyindexvar.hash
+				h0 := filter.geth0(hash)
+				h1 := filter.geth1(h0, hash)
+				h2 := filter.geth2(hash)
+				stack[stacksize] = keyindexvar
+				stacksize++
+				sets0[h0].xormask ^= hash
+				sets0[h0].count--
+				if sets0[h0].count == 1 {
+					Q0[Q0size].index = h0
+					Q0[Q0size].hash = sets0[h0].xormask
+					Q0size++
+				}
+				sets0[h1].xormask ^= hash
+				sets0[h1].count--
+				if sets0[h1].count == 1 {
+					Q0[Q0size].index = h1
+					Q0[Q0size].hash = sets0[h1].xormask
+					Q0size++
+				}
+				sets1[h2].xormask ^= hash
+				sets1[h2].count--
+				if sets1[h2].count == 1 {
+					Q1[Q1size].index = h2
+					Q1[Q1size].hash = sets1[h2].xormask
+					Q1size++
+				}
+			}
+			if Q1size > 0 {
+				Q1size--
+				keyindexvar := Q1[Q1size]
+				index := keyindexvar.index
+				if sets1[index].count == 0 {
+					continue
+				}
+				sets1[index].count = 0
+				hash := keyindexvar.hash
+				h0 := filter.geth0(hash)
+				h1 := filter.geth1(h0, hash)
+				keyindexvar.index += filter.BlockLength
+
+				stack[stacksize] = keyindexvar
+				stacksize++
+				sets0[h0].xormask ^= hash
+				sets0[h0].count--
+				if sets0[h0].count == 1 {
+					Q0[Q0size].index = h0
+					Q0[Q0size].hash = sets0[h0].xormask
+					Q0size++
+				}
+				sets0[h1].xormask ^= hash
+				sets0[h1].count--
+				if sets0[h1].count == 1 {
+					Q0[Q0size].index = h1
+					Q0[Q0size].hash = sets0[h1].xormask
+					Q0size++
+				}
+
+			}
+		}
+
+		if stacksize == size {
+			// success
+			break
+		}
+
+		for i := range sets0 {
+			sets0[i] = xorset{0, 0}
+		}
+		for i := range sets1 {
+			sets1[i] = xorset{0, 0}
+		}
+		filter.Seed = splitmix64(&rngcounter)
+	}
+
+	stacksize := size
+	for stacksize > 0 {
+		stacksize--
+		ki := stack[stacksize]
+		val := uint8(fingerprint(ki.hash))
+		if ki.index < filter.BlockLength {
+			h0 := filter.geth0(ki.hash)
+			h1 := filter.geth1(h0, ki.hash)
+			h2 := filter.geth2(ki.hash)
+			val ^= filter.Fingerprints[h0] ^ filter.Fingerprints[h1] ^ filter.Fingerprints[h2+filter.BlockLength]
+		} else {
+			h0 := filter.geth0(ki.hash)
+			val ^= filter.Fingerprints[h0] ^ filter.Fingerprints[filter.geth1(h0, ki.hash)]
+		}
+		filter.Fingerprints[ki.index] = val
+	}
+	return filter, nil
+}
---
+++ twosegments_test.go
@@ -0,0 +1,66 @@
+package xorfilter
+
+import (
+	"fmt"
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestBasicTwoSegment(t *testing.T) {
+	testsize := 10000
+	keys := make([]uint64, testsize, testsize)
+	for i := range keys {
+		keys[i] = splitmix64(&rng)
+	}
+	filter,_ := PopulateTwoSegment(keys)
+	for _, v := range keys {
+		assert.Equal(t, true, filter.Contains(v))
+	}
+	falsesize := 1000000
+	matches := 0
+	bpv := float64(len(filter.Fingerprints)) * 8.0 / float64(testsize)
+	fmt.Println("Xor8TwoSegment filter:")
+	fmt.Println("bits per entry ", bpv)
+	assert.Equal(t, true, bpv < 10.)
+	for i := 0; i < falsesize; i++ {
+		v := splitmix64(&rng)
+		if filter.Contains(v) {
+			matches++
+		}
+	}
+	fpp := float64(matches) * 100.0 / float64(falsesize)
+	fmt.Println("false positive rate ", fpp)
+	assert.Equal(t, true, fpp < 0.40)
+}
+
+func BenchmarkPopulateTwoSegment100000(b *testing.B) {
+	testsize := 10000
+	keys := make([]uint64, testsize, testsize)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+		for i := range keys {
+			keys[i] = splitmix64(&rng)
+		}
+		b.StartTimer()
+		PopulateTwoSegment(keys)
+	}
+}
+
+func BenchmarkContainsTwoSegment100000(b *testing.B) {
+	testsize := 10000
+	keys := make([]uint64, testsize, testsize)
+	for i := range keys {
+		keys[i] = splitmix64(&rng)
+	}
+	filter,_ := PopulateTwoSegment(keys)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		filter.Contains(keys[n%len(keys)])
+	}
+}
